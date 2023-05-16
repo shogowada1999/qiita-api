557feb2d8610a4593ec71a3c6035ddba770021f8
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2023-05-16 23:05:01 +0900

    feat: main の処理と Makefile にコマンドを追加

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 .PHONY: get-all
 get-all:
-	go run main.go
+	go run main.go GET_ALL
+
+.PHONY: get
+get:
+	go run main.go GET ${ID}
+
--- main.go
+++ main.go
@@ -4,37 +4,62 @@ import (
 	"fmt"
 	"os"
 
-	"qiita-api/methods"
+	"qiita-api/pkg/article"
 )
+
+const errMessage = "Error: The command has too many or too few arguments."
 
 func main() {
 	argsLen := len(os.Args)
-	if !(argsLen >= 1 && argsLen <= 3) {
-		fmt.Println("Error: The command has too many or too few arguments.")
+	if !(argsLen >= 2 && argsLen <= 3) {
+		fmt.Println(errMessage)
 		return
 	}
 
 	method := os.Args[1]
 	switch method {
 	case "GET_ALL":
-		methods.GetAll()
+		article.GetAll()
 	case "GET":
-		//
+		if argsLen != 3 {
+			fmt.Println(errMessage)
+			return
+		}
+		article.Get(os.Args[2])
 	case "POST":
-		//
+		if argsLen != 3 {
+			fmt.Println(errMessage)
+			return
+		}
+		// TODO: 投稿処理
 	case "PATCH":
-		//
+		if argsLen != 3 {
+			fmt.Println(errMessage)
+			return
+		}
+		// TODO: 更新処理
 	case "DELETE":
-		//
+		if argsLen != 3 {
+			fmt.Println(errMessage)
+			return
+		}
+		// TODO: 削除処理
 	case "SYNC_ALL":
-		//
+		if argsLen != 2 {
+			fmt.Println(errMessage)
+			return
+		}
+		// TODO: 一括同期処理
 	case "SYNC":
-		//
+		if argsLen != 2 {
+			fmt.Println(errMessage)
+			return
+		}
+		// TODO: 同期処理
 	default:
 		fmt.Println("Error: An invalid command was entered.")
 		return
 	}
 
 	fmt.Println("Process finished.")
-	return
 }
