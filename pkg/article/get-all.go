5653089fcbf639f9cd1736c52de433aff0adb695
shogowada1999 <81617513+shogowada1999@users.noreply.github.com>
2023-05-16 23:05:39 +0900

    refactor: get-all の処理をリファクタリング

---
+++ pkg/article/get-all.go
@@ -0,0 +1,32 @@
+package article
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func GetAll() {
+	url := URL + "/authenticated_user/items"
+	bodyBytes, err := request("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	var articles []Article
+
+	json.Unmarshal([]byte(bodyBytes), &articles)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	for i := range articles {
+		articles[i].FormatDates()
+		articles[i].TruncateTitle()
+	}
+
+	for _, article := range articles {
+		printArticle(article)
+	}
+}
